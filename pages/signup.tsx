311608291750ee82be158478841bdf5586a9ecff
braydentw <reshiramfire130@gmail.com>
2021-07-30 21:21:47 -0500

    Added Formik

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,172 +1,170 @@
 import router from 'next/router'
 import React, { useState } from 'react'
+import * as Yup from 'yup'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { useAuth } from '../lib/auth'
+import FocusError from '../components/utility/FocusError'
 
 function signup() {
   const auth = useAuth()
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const createUserWithEmailAndPasswordHandler = (
-    event,
-    name,
-    email,
-    password
-  ) => {
-    event.preventDefault()
-    auth.createWithPassword(name, email, password)
-    setName('')
-    setEmail('')
-    setPassword('')
-  }
-
   if (auth.user) {
     router.push('/')
   }
-  const onChangeHandler = (e) => {
-    const { name, value } = e.currentTarget
-    if (name === 'email') {
-      setEmail(value)
-    } else if (name === 'password') {
-      setPassword(value)
-    } else if (name === 'name') {
-      setName(value)
-    }
-  }
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <img
-          className="mx-auto h-12 w-auto"
-          src="/img/logos/yei.png"
-          alt="Workflow"
-        />
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Create an Account
-        </h2>
-      </div>
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+        password: '',
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        auth.createWithPassword(values.name, values.email, values.password)
+        resetForm({})
+        setSubmitting(false)
+      }}
+    >
+      <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            className="mx-auto h-12 w-auto"
+            src="/img/logos/yei.png"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Create an Account
+          </h2>
+        </div>
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                First Name
-              </label>
-              <div className="mt-1">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  autoComplete="name"
-                  value={name}
-                  placeholder="John"
-                  onChange={(e) => onChangeHandler(e)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yei-primary-main focus:yei-primary-main sm:text-sm"
-                  required
-                />
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <Form className="space-y-3">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  First Name
+                </label>
+                <div className="mt-1">
+                  <Field
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder="John"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yei-primary-main focus:yei-primary-main sm:text-sm"
+                    required
+                  />
+                  <ErrorMessage
+                    className="formik-error text-sm"
+                    component="div"
+                    name="name"
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email address
-              </label>
-              <div className="mt-1">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  value={email}
-                  placeholder="E.g: Faruq"
-                  onChange={(e) => onChangeHandler(e)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yei-primary-main focus:yei-primary-main sm:text-sm"
-                  required
-                />
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <Field
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="im@batman.com"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yei-primary-main focus:yei-primary-main sm:text-sm"
+                    required
+                  />
+                  <ErrorMessage
+                    className="formik-error text-sm"
+                    component="div"
+                    name="email"
+                  />
+                </div>
               </div>
-            </div>
 
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <div className="mt-1">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="password"
-                  value={password}
-                  onChange={(e) => onChangeHandler(e)}
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yei-primary-main focus:border-yei-primary-main sm:text-sm"
-                  required
-                />
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="mt-1">
+                  <Field
+                    id="password"
+                    name="password"
+                    type="password"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yei-primary-main focus:border-yei-primary-main sm:text-sm"
+                    required
+                  />
+                  <ErrorMessage
+                    className="formik-error text-sm"
+                    component="div"
+                    name="password"
+                  />
+                </div>
               </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                onClick={(event) => {
-                  createUserWithEmailAndPasswordHandler(
-                    event,
-                    name,
-                    email,
-                    password
-                  )
-                }}
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yei-primary-darker hover:bg-yei-primary-darker focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yei-primary-main"
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
-
-          <span className="text-red-500 text-xs">{auth.authError}</span>
-
-          <div className="mt-6">
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-300" />
-              </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="px-2 bg-white text-gray-500">
-                  Or continue with
-                </span>
-              </div>
-            </div>
-
-            <div className="mt-6 grid">
+              <br />
               <div>
                 <button
-                  className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                  onClick={() => auth.signinWithGoogle()}
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-yei-primary-darker hover:bg-yei-primary-darker focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yei-primary-main"
                 >
-                  Google
+                  Sign Up
                 </button>
-                <button
-                  className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                  onClick={() => auth.signout()}
-                >
-                  Sign Out
-                </button>
+              </div>
+              <FocusError />
+            </Form>
+
+            <span className="text-red-500 text-xs">{auth.authError}</span>
+
+            <div className="mt-6">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center text-sm">
+                  <span className="px-2 bg-white text-gray-500">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+
+              <div className="mt-6 grid">
+                <div>
+                  <button
+                    className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                    onClick={() => auth.signinWithGoogle()}
+                  >
+                    Google
+                  </button>
+                  <button
+                    className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                    onClick={() => auth.signout()}
+                  >
+                    Sign Out
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Formik>
   )
 }
 
 export default signup
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string().max(50, 'Name too long.').required('Name required.'),
+  email: Yup.string().email('Invalid email.').required('Email required.'),
+  password: Yup.string()
+    .min(6, 'Password must be at least six characters long.')
+    .required('Password is required.'),
+})
