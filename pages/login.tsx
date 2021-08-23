577a6b7325013f224d41296886c8276c37064534
braydentw <reshiramfire130@gmail.com>
2021-08-23 14:28:31 -0500

    Removed sign out button in login

--- pages/login.tsx
+++ pages/login.tsx
@@ -121,12 +121,6 @@ export default function Login() {
                   >
                     Google
                   </button>
-                  <button
-                    className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                    onClick={() => auth.signout()}
-                  >
-                    Sign Out
-                  </button>
                 </div>
               </div>
             </div>
