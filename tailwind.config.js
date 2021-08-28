29b885f40440c176cc2cdac82016b2d58078f569
braydentw <reshiramfire130@gmail.com>
2021-08-28 14:29:48 -0500

    Added blue and green to tailwind palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,8 @@ module.exports = {
       indigo: colors.indigo,
       red: colors.rose,
       yellow: colors.amber,
+      blue: colors.blue,
+      green: colors.green,
     },
     extend: {
       colors: {
