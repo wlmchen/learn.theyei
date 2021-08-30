1142fb0a6be844d08d87746ff1ac0a790dbe5459
braydentw <reshiramfire130@gmail.com>
2021-08-30 13:04:31 -0500

    Fixed nextCategory and replaced with nextChapter

--- components/category/PrevNextBar.tsx
+++ components/category/PrevNextBar.tsx
@@ -85,17 +85,17 @@ function PrevNextBar({ kebabCategories, kebabChapters, slug }) {
       ) : (
         <>
           {nextModule === 'slides' ? (
-            <Link href={`/category/${slug[0]}/${nextCategory}/${nextModule}`}>
+            <Link href={`/category/${slug[0]}/${nextChapter}/${nextModule}`}>
               <span className="flex items-center font-semibold text-gray-600 cursor-pointer">
-                <ChevronLeftIcon className="w-5 h-5 mr-1" />
-                Prev
+                Next
+                <ChevronRightIcon className="w-5 h-5 ml-1" />
               </span>
             </Link>
           ) : (
             <Link href={`/category/${slug[0]}/${slug[1]}/${nextModule}`}>
               <span className="flex items-center font-semibold text-gray-600 cursor-pointer">
-                <ChevronLeftIcon className="w-5 h-5 mr-1" />
-                Prev
+                Next
+                <ChevronRightIcon className="w-5 h-5 ml-1" />
               </span>
             </Link>
           )}
